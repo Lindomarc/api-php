6837a38688f7e6ac5e45a09ac8dbc3425b57ca27
lindomarc <lindomarc@outlook.com>
2022-01-17 08:59:35 -0300

    first commit

---
+++ api/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$data = [];
+
+// request
+if(isset($_GET['option'])){
+    switch ($_GET['option']) {
+        case 'status':
+            $data['status'] = 'success';
+            $data['data'] = 'API OK';            
+            break;
+        
+        default:
+            $data['status'] = 'ERROR'        ;
+            break;
+    }
+} else {
+    $data['status'] = 'ERROR';
+
+}
+
+// emitir response da api
+response($data);
+
+// construir response
+function response($dataResponse)
+{
+    header('Content-Type:application/json');
+    echo json_encode($dataResponse);
+}
